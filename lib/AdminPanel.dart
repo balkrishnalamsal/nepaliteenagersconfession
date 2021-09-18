dd66bbb9340ec8c350ddbb3c5ca62fc26bb96927
adwin <adwinlamal@gmail.com>
2021-09-18 16:29:35 +0545

    Dayy four

--- lib/AdminPanel.dart
+++ lib/AdminPanel.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nepaliteenagersconfession/AdminPanel.dart';
 import 'package:nepaliteenagersconfession/Createpost.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:uuid/uuid.dart';
 
-class HomePage extends StatefulWidget {
+class Adminpanel extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _AdminpanelState createState() => _AdminpanelState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _AdminpanelState extends State<Adminpanel> {
   Timestamp? iteam;
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
@@ -72,21 +69,16 @@ class _HomePageState extends State<HomePage> {
           decoration: BoxDecoration(color: Colors.deepOrangeAccent),
           child: Column(
             children: [
-              GestureDetector(
-                onDoubleTap: (){
-                  controlSignIN();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 50),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.15,
-                    width: MediaQuery.of(context).size.height * 0.5,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assets/giveone.png")),
-                        color: Colors.deepOrange,
-                        shape: BoxShape.circle),
-                  ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  width: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/giveone.png")),
+                      color: Colors.deepOrange,
+                      shape: BoxShape.circle),
                 ),
               ),
               Padding(
@@ -119,7 +111,9 @@ class _HomePageState extends State<HomePage> {
               stream: FirebaseFirestore.instance
                   .collection("Confession")
                   .orderBy("Time", descending: true)
-                  .where("status", isEqualTo: "Approved")
+                  .where(
+                    "status",isEqualTo: "Pending"
+                  )
                   .snapshots()
                   .asBroadcastStream(),
               builder: (BuildContext context,
@@ -313,86 +307,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-  }
-
-  Future<Null> controlSignIN() async {
-    preferences = await SharedPreferences.getInstance();
-    GoogleSignInAccount? googleuser = await googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication =
-        await googleuser!.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken);
-
-    User? firebaseUser =
-        (await firebaseAuth.signInWithCredential(credential)).user;
-
-    if (firebaseUser != null) {
-      final QuerySnapshot resutquery = await FirebaseFirestore.instance
-          .collection("profile")
-          .where("userrid", isEqualTo: firebaseUser.uid)
-          .get();
-
-      final List<DocumentSnapshot> documentsnapshot = resutquery.docs;
-
-      //Save data to firestore -if new user
-      if (documentsnapshot.length == 0) {
-        String postid = Uuid().v4();
-        FirebaseFirestore.instance
-            .collection("profile")
-            .doc(firebaseUser.uid)
-            .set({
-          "nickname": firebaseUser.displayName,
-          "photourl": firebaseUser.photoURL,
-          "userrid": firebaseUser.uid,
-          "email": firebaseUser.email,
-          "createdAt": DateTime.now().millisecondsSinceEpoch.toString(),
-          "unique": postid,
-        });
-
-        //Write data to local
-        currentuser = firebaseAuth;
-        await preferences.setString("email", currentuser.currentUser!.email!);
-        await preferences.setString("id", currentuser.currentUser!.uid);
-        await preferences.setString("unique", postid);
-        await preferences.setString(
-            "nickname", currentuser.currentUser!.displayName!);
-        await preferences.setString(
-            "photourl", currentuser.currentUser!.photoURL!);
-      } else {
-        currentuser = firebaseAuth;
-        await preferences.setString("email", documentsnapshot[0]["email"]);
-        await preferences.setString("id", documentsnapshot[0]["userrid"]);
-        await preferences.setString("unique", documentsnapshot[0]["unique"]);
-        await preferences.setString(
-            "nickname", documentsnapshot[0]["nickname"]);
-        await preferences.setString(
-            "photourl", documentsnapshot[0]["photourl"]);
-      }
-
-      this.setState(() {
-        isloading = false;
-
-        String? value = preferences.getString("email");
-        if (value == "adwinlamal@gmail.com" ||
-            value == "balkrishnalamsal12@gmail.com" ||
-            value == "balkrishnalamsal1@gmail.com") {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => Adminpanel()));
-        } else {
-          Fluttertoast.showToast(
-            msg: 'Not available',
-            toastLength: Toast.LENGTH_LONG,
-            gravity: ToastGravity.BOTTOM,
-            backgroundColor: Colors.orange,
-            textColor: Colors.black,
-          );
-        }
-      });
-    } else {
-      Fluttertoast.showToast(msg: "login Failed");
-      this.setState(() {});
-    }
   }
 }
--- lib/Createpost.dart
+++ lib/Createpost.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -12,14 +12,14 @@ class CreatePost extends StatefulWidget {
 
 class _CreatePostState extends State<CreatePost> {
   TextEditingController? squadcontroller;
-  String ? value;
+  String? value;
+  String? loading = "False";
+  String? button = "False";
 
-
-
-@override
+  @override
   void initState() {
     super.initState();
-    squadcontroller=TextEditingController();
+    squadcontroller = TextEditingController();
   }
 
   @override
@@ -33,31 +33,35 @@ class _CreatePostState extends State<CreatePost> {
                 Padding(
                   padding: const EdgeInsets.only(top: 50),
                   child: Container(
-                    height: MediaQuery.of(context).size.height*0.15,
-                    width: MediaQuery.of(context).size.height*0.5,
+                    height: MediaQuery.of(context).size.height * 0.15,
+                    width: MediaQuery.of(context).size.height * 0.5,
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: AssetImage("assets/giveone.png")
-                        ),
+                            image: AssetImage("assets/giveone.png")),
                         color: Colors.deepOrange,
-                        shape: BoxShape.circle
-                    ),
+                        shape: BoxShape.circle),
                   ),
                 ),
-
                 Padding(
                   padding: const EdgeInsets.only(top: 15.0),
-                  child: Row(mainAxisAlignment:MainAxisAlignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Nepalese Teenagers Confession",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                      Text(
+                        "Nepalese Teenagers Confession",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Icon(Icons.share,color: Colors.white,),
+                        child: Icon(
+                          Icons.share,
+                          color: Colors.white,
+                        ),
                       )
                     ],
                   ),
                 ),
-
                 Container(
                     padding: EdgeInsets.all(10),
                     child: CupertinoTextField(
@@ -70,34 +74,75 @@ class _CreatePostState extends State<CreatePost> {
                             BoxShadow(color: Colors.grey, blurRadius: 1)
                           ]),
                       maxLines: 10,
-                      controller:squadcontroller,
+                      controller: squadcontroller,
                       placeholderStyle: TextStyle(color: Colors.grey),
                       placeholder: "What's on your mind ?",
                     )),
+                (button == "False")
+                    ? Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Center(
+                            child: CupertinoButton(
+                                color: Colors.black,
+                                onPressed: () async {
+                                  Navigator.of(context);
+                                  loading = "True";
+                                  button = "True";
+                                  String post = Uuid().v4();
+                                  FirebaseFirestore.instance
+                                      .collection("Confession")
+                                      .doc(post)
+                                      .set({
+                                    "description": squadcontroller!.text,
+                                    "post": post,
+                                    "status": "Pending",
+                                    "Time": DateTime.now(),
+                                  }).whenComplete(() {
+                                    setState(() {
+                                      Fluttertoast.showToast(msg: 'Successfully sent for approval',
+                                        toastLength: Toast.LENGTH_LONG,
+                                        gravity: ToastGravity.BOTTOM,
+                                        backgroundColor: Colors.orange,
+                                        textColor: Colors.black,
+                                      );
+                                      button = "False";
+                                      loading = "False";
+                                      squadcontroller!.clear();
+                                    });
+
+                                  });
+                                },
+                                child: Text(
+                                  "Send for Approval",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.white),
+                                ))),
+                      )
+                    : Text(""),
+                (loading == "False")
+                    ? Text(
+                        "",
+                        style: TextStyle(fontSize: 0),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Container(
+                            width: 100,
+                            child: LinearProgressIndicator(
+                              valueColor: new AlwaysStoppedAnimation<Color>(
+                                  Colors.black),
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
                 Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Center(
-                      child: CupertinoButton(
-                          color: Colors.black,
-                          onPressed: ()async{
-                            String post = Uuid().v4();
-                               FirebaseFirestore.instance.collection("Confession").doc(post).set({
-                              "description":squadcontroller!.text,
-                              "post":post,
-                              "Time":DateTime.now(),
-                            }).whenComplete(()
-                            {
-                              squadcontroller!.clear();
-                            }
-                            );
-                          },
-                          child: Text(
-                            "POST",
-                            style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                color: Colors.white),
-                          ))),
-                ),
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                      "Note: Your confession will be posted as soon as admin approved the post"),
+                )
               ],
             ),
           ),
